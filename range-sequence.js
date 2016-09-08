dcb2069b5414cfd3e818ed4e86f243ceead67faf
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-09-07 21:20:55 -0700

    finished with help

--- range-sequence.js
+++ range-sequence.js
@@ -1,8 +1,14 @@
 // Range sequence
 // Recursively produce a sequence of numbers from 3 to 9 with exclusive bounds.
 
-function range(begin, end) {
+// redo
 
+function range(a, b) {
+  if (a + 1 === b || a >= b) {
+  	return [];
+  } else {
+  	return [a + 1].concat(range(a + 1, b));
+  }
 }
 
 
